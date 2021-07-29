5d6cea406f9d8f18d071d3b6e6a117c7e3d5d0ff
pouyanzar <pouyan.zarbafian@hotmail.com>
2021-07-29 01:40:21 +0000

    add CTRL+C interruption to play.js

--- play.js
+++ play.js
@@ -1,4 +1,20 @@
+const { stdin } = require("process");
 const {connect} = require("./client");
+// setup interface to handle user input from stdin
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+stdin.on('data', (key) => handleUserInput(key));
+const handleUserInput = (key) => {
+  if (key === '\u0003')
+    process.exit();
+}
 console.log("Connecting ...");
 connect();
+setupInput();
