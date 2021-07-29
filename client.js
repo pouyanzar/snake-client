38b45fa7ca620fb469ce053996867086b02da0f9
pouyanzar <pouyan.zarbafian@hotmail.com>
2021-07-29 01:10:05 +0000

    more events added

--- client.js
+++ client.js
@@ -9,6 +9,11 @@ const connect = function () {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+    conn.write('Name: POU');
+    // conn.write("Move: up");
+  });
   conn.on("data", (data) => console.log(data));
   return conn;
 };
